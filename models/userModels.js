5ef5cec1b21ff539482527c7e054985b7e67c973
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-24 00:15:17 +0100

    updated userschema

--- models/userModels.js
+++ models/userModels.js
@@ -60,6 +60,12 @@ userSchema.pre('save', async function(next){
     next()
 })
 
+userSchema.pre('save', function(next){
+    if (!this.isModified('password') || this.isNew) return next()
+
+    this.passwordChangedAt=Date.now() - 1000
+    next()
+})
 
 userSchema.methods.changePasswordAfter = (JWTTimestamp) => {
     if (this.passwordChangedAt){
